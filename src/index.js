db4b7f0e197f4d1c779937910adcad81b5c4ea55
Tina <TinaNguyen290@yahoo.com>
2020-12-24 23:54:07 -0800

    Summary: Arrow functions DONT REBIND the "this" keyword

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-const square1 = function(number){
-  return number * number;
-}
+const person = {
+  talk(){
+    var self = this;
+    setTimeout(() => {
+      //this returns a reference to the window object
+      console.log("this", this);
 
-// new function syntax, more compact and cleaner
-const square2 = number => number * number;
+    },1000);
+    
+  }
+};
 
-console.log(square1(3));
-
-console.log(square2(5));
-
-
-const jobs = [
-  {id: 1, isActive: true},
-  {id: 2, isActive: true},
-  {id: 3, isActive: false},
-];
-
-
-//filter iterates through the jobs array, 'job' is every single element
-//in the array, and returns of that current job is active
-const activeJobs1 = jobs.filter(function(job){ return job.isActive ;})
-
-//cleaner version of above
-const activeJobs2 = jobs.filter(job => job.isActive );
+person.talk();
