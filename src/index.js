0435143e850429fb8d0e67fb07d175b34caaabd7
Tina <TinaNguyen290@yahoo.com>
2020-12-25 00:29:39 -0800

    Refresher about class inheritance; it's about the same as when I used Java, was weak on "super constructors"

--- src/index.js
+++ src/index.js
@@ -8,3 +8,19 @@ class Person {
 }
 
 const person = new Person("Kyoko");
+
+
+class Teacher extends Person{
+  constructor(name, degree) {
+    //reference the parent, which is required
+    super(name);
+    this.degree = degree;
+
+  }
+
+  teach(){
+    console.log("teach");
+  }
+}
+
+const teacher = new Teacher("KyokoTeacher", "BS");
