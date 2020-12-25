5873ea2d59fa523b0d68bc21e464bf0473f3e2a8
Tina <TinaNguyen290@yahoo.com>
2020-12-25 00:46:54 -0800

    Learned the differences between Named and Default Exports and how to combine them

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { Teacher } from "./teacher";
+import Teacher, {promote} from "./teacher";
+//import React, { Component } from 'react';
+// Default -> import ... from '';
+// Named -> import {...} from '';
 
 const teacher = new Teacher("KyokoTeacher", "BS");
 
--- src/teacher.js
+++ src/teacher.js
@@ -1,6 +1,10 @@
 import { Person } from './person';
 
-export class Teacher extends Person{
+//named export
+export function promote(){};
+
+//default export
+export default class Teacher extends Person{
     constructor(name, degree) {
       //reference the parent, which is required
       super(name);
