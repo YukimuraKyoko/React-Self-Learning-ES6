6390febdf4bfdabd781955e2e828b696334812f7
Tina <TinaNguyen290@yahoo.com>
2020-12-24 23:18:37 -0800

    Restarted project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// var -> function
+// let -> block
+// const -> block
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function sayHello(){
+  for(let i = 0; i < 5; i++){
+    console.log(i);
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+sayHello();
