db98c7f0671192029da2d4916b04901a867c6644
Tina <TinaNguyen290@yahoo.com>
2020-12-25 00:10:11 -0800

    spread operator pt 1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-const address = {
-  street: '',
-  city: '',
-  country: ''
-};
-/*
-const street = address.street;
-const city = address.city;
-const country = address.country;
-*/
+const first = [1,2,3];
+const second = [4,5,6];
 
-//Object Destructuring, the same as lines 7-9
-const {street, city, country} = address;
+//old way
+const combined1 = first.concat(second);
 
-//Object Destructuring street named as 'st'
-const {street: st} = address;
+//'...' 3 dots is the spread operator
+const combined2 = [...first, ...second];
+
+//you can insert individual elements in between arrays
+const combined3 = [...first, 'a', ...second, 'b'];
+
+//the exact same as first
+const clone = [...first];
